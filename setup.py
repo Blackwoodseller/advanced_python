dc4c5e02d64b8c8d95bae8aa0002c859337c6a07
alp <refog@i.ua>
2019-02-27 00:13:55 +0300

    Homework10:  Oleksii Potorzhynskyi

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(
+    name="calculator",
+    version="0.0.1",
+    author="Oleksii Potorzhynskyi",
+    author_email="oleksii_potorzhynskyi@epam.com",
+    url="http://www.epam.com",
+    description="Money expression calculator example",
+    packages=["calculator"],
+)
